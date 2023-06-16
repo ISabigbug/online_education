7171bfdf9850d2c672b0b4f32a8ce1508952dbeb
ISabigbug <121659902+ISabigbug@users.noreply.github.com>
2023-06-16 14:30:59 +0800

    前端代码

--- vue-admin-template-master/config/prod.env.js
+++ vue-admin-template-master/config/prod.env.js
@@ -1,6 +1,5 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  // BASE_API: '"https://easy-mock.com/mock/5950a2419adc231f356a6636/vue-admin"',
-  BASE_API: '"http://124.223.161.242:9002"'
+  BASE_API: '"http://###.###.###.###:####"'
 }
